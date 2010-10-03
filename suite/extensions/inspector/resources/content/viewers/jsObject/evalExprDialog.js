9e969e386f1a9b704dd5ee72706610430b9bef2c
Colby Russell <sevenspade@gmail.com>
2010-10-03 12:48:07 -0500

    Bug 391955 - Missing string jsObjectExpressionError.title in inspector.properties; r=neil

--- suite/extensions/inspector/resources/content/viewers/jsObject/evalExprDialog.js
+++ suite/extensions/inspector/resources/content/viewers/jsObject/evalExprDialog.js
@@ -36,13 +36,14 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-/***************************************************************
-* EvalExprDialog --------------------------------------------
-*  A dialog for entering javascript expression to evaluate and
-* view in the JS Object Viewer.
-* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+/*****************************************************************************
+* EvalExprDialog -------------------------------------------------------------
+*   A dialog for entering javascript expression to evaluate and view in the JS
+*   Object Viewer.
+* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 * REQUIRED IMPORTS:
-****************************************************************/
+* chrome://inspector/content/jsutil/xpcom/XPCU.js
+*****************************************************************************/
 
 var gViewer = window.arguments[0];
 var gTarget = window.arguments[1];
@@ -56,12 +57,13 @@ function execute()
   var rad = document.getElementById("inspect-new-window");
   try {
     gViewer.doEvalExpr(txf.value, gTarget, rad.selected);
-  } catch (ex) {
+  }
+  catch (ex) {
     // alert the user of an error in their expression, and don't close
-    Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-              .getService(Components.interfaces.nsIPromptService)
-                .alert(window, document.getElementById("strings")
-                               .getString("jsObjectExpressionError.title"), ex);
+    let svc = XPCU.getService("@mozilla.org/embedcomp/prompt-service;1",
+                              "nsIPromptService");
+    svc.alert(window, ex.name, ex.message);
+
     return false;
   }
   return true;
