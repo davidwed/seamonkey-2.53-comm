25349265b74e5ac6544b0843fd4b45c200955713
Tooru Fujisawa <arai_a@mac.com>
2017-02-20 12:58:22 +0900

    Bug 1340947 - Part 5: Use for-of when iterating over fixIterator in mail/test/mozmill/. r=jorgk

--- mailnews/base/content/virtualFolderProperties.js
+++ mailnews/base/content/virtualFolderProperties.js
@@ -52,7 +52,7 @@ function onLoad()
 
     if (windowArgs.searchTerms) // then add them to our search session
     {
-      for (let searchTerm in fixIterator(windowArgs.searchTerms,
+      for (let searchTerm of fixIterator(windowArgs.searchTerms,
                                          Ci.nsIMsgSearchTerm))
         gSearchTermSession.appendTerm(searchTerm);
     }
