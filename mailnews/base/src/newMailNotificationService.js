63dea0f657a6e2dbf3e81f2069485cac04d0b880
Tooru Fujisawa <arai_a@mac.com>
2017-02-20 12:58:22 +0900

    Bug 1340947 - Part 6: Use for-of when iterating over fixIterator in mailnews/. r=jorgk

--- mailnews/base/src/newMailNotificationService.js
+++ mailnews/base/src/newMailNotificationService.js
@@ -138,7 +138,7 @@ NewMailNotificationService.prototype = {
     }
 
     let allFolders = folder.descendants;
-    for (let folder in fixIterator(allFolders, Ci.nsIMsgFolder)) {
+    for (let folder of fixIterator(allFolders, Ci.nsIMsgFolder)) {
       if (this.confirmShouldCount(folder)) {
         let count = folder.getNumUnread(false);
         this._log.debug("NMNS_countUnread: folder " + folder.URI + ", " + count + " unread");
@@ -231,7 +231,7 @@ NewMailNotificationService.prototype = {
           newCount += folderNew;
       }
 
-      for (let folder in fixIterator(allFolders, Ci.nsIMsgFolder)) {
+      for (let folder of fixIterator(allFolders, Ci.nsIMsgFolder)) {
         if (this.confirmShouldCount(folder)) {
           let folderNew = folder.getNumNewMessages(false);
           this._log.debug("NMNS_biffStateChanged: folder " + folder.URI + " new messages: " + folderNew);
