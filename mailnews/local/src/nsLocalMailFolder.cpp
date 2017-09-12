c5b94d76e49c5518a843bff3943c8daf3366aacc
Jorg K <jorgk@jorgk.com>
2017-09-12 13:40:19 +0200

    Bug 1340972 - Part 2: Fix use of NS_ADDREF() and NS_IF_ADDREF() in mailnews/local. r=aceman

--- mailnews/local/src/nsLocalMailFolder.cpp
+++ mailnews/local/src/nsLocalMailFolder.cpp
@@ -3745,7 +3745,7 @@ nsMsgLocalMailFolder::AddMessageBatch(uint32_t aMessageCount,
       newMailParser->EndMsgDownload();
       hdrArray->AppendElement(newHdr);
     }
-    NS_ADDREF(*aHdrArray = hdrArray);
+    hdrArray.forget(aHdrArray);
   }
   ReleaseSemaphore(static_cast<nsIMsgLocalMailFolder*>(this));
   return rv;
