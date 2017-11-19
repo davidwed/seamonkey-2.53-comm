75411d7165e8a96e199745ef0d36dbb50da8ec51
Florian Queze <florian@queze.net>
2017-11-19 10:54:56 +0100

    Bug 1356569 - Remove notifyObservers' last parameter when it is falsy, rs=Fallen.

--- mail/base/content/sanitize.js
+++ mail/base/content/sanitize.js
@@ -169,7 +169,7 @@ Sanitizer.prototype = {
         try {
           var os = Cc["@mozilla.org/observer-service;1"]
                      .getService(Ci.nsIObserverService);
-          os.notifyObservers(null, "browser:purge-session-history", "");
+          os.notifyObservers(null, "browser:purge-session-history");
         } catch (e) { }
 
         try {
--- suite/components/profile/content/profileSelection.js
+++ suite/components/profile/content/profileSelection.js
@@ -129,7 +129,7 @@ function AcceptDialog()
                      .createInstance(Ci.nsISupportsPRBool);
   Cc["@mozilla.org/observer-service;1"]
     .getService(Ci.nsIObserverService)
-    .notifyObservers(cancelQuit, "quit-application-requested", null);
+    .notifyObservers(cancelQuit, "quit-application-requested");
   if (cancelQuit.data)
     return false;
 
