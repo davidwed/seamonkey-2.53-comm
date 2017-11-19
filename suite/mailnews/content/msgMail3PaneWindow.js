2fc58dc83608323077f8e1e5309d8a8cfb0ef144
Florian Queze <florian@queze.net>
2017-11-19 10:54:56 +0100

    Bug 1356569 - Remove addObserver's last parameter when it is false, rs=Fallen.

--- suite/mailnews/content/msgMail3PaneWindow.js
+++ suite/mailnews/content/msgMail3PaneWindow.js
@@ -671,8 +671,8 @@ function OnLoadMessenger()
 {
   AddMailOfflineObserver();
   CreateMailWindowGlobals();
-  Services.prefs.addObserver("mail.pane_config.dynamic", MailPrefObserver, false);
-  Services.prefs.addObserver("mail.showCondensedAddresses", MailPrefObserver, false);
+  Services.prefs.addObserver("mail.pane_config.dynamic", MailPrefObserver);
+  Services.prefs.addObserver("mail.showCondensedAddresses", MailPrefObserver);
   UpdateMailPaneConfig();
   Create3PaneGlobals();
   verifyAccounts(null, false);
@@ -704,8 +704,7 @@ function OnLoadMessenger()
   let tabmail = GetTabMail();
   tabmail.registerTabType(gMailNewsTabsType);
   tabmail.openFirstTab();
-  Services.obs.addObserver(MailWindowIsClosing,
-                           "quit-application-requested", false);
+  Services.obs.addObserver(MailWindowIsClosing, "quit-application-requested");
 
   InitMsgWindow();
   messenger.setWindow(window, msgWindow);
