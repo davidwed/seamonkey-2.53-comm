9da6286ef2bbfaf55ccf72eb00a074d707d563b5
Jorg K <jorgk@jorgk.com>
2017-12-05 23:20:37 +0100

    Bug 1408175 - pass nsCString to MsgStripQuotedPrintable() instead of a raw char pointer. r=bz

--- mailnews/base/util/nsMsgDBFolder.cpp
+++ mailnews/base/util/nsMsgDBFolder.cpp
@@ -5651,9 +5651,7 @@ void nsMsgDBFolder::decodeMsgSnippet(const nsACString& aEncodingType, bool aIsCo
   }
   else if (MsgLowerCaseEqualsLiteral(aEncodingType, ENCODING_QUOTED_PRINTABLE))
   {
-    // giant hack - decode in place, and truncate string.
-    MsgStripQuotedPrintable((unsigned char *) aMsgSnippet.get());
-    aMsgSnippet.SetLength(strlen(aMsgSnippet.get()));
+    MsgStripQuotedPrintable(aMsgSnippet);
   }
 }
 
