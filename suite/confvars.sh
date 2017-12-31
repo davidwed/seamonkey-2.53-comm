6024bc726894fc82d3ef05779ee91a630beea0cf
Tom Prince <mozilla@hocat.ca>
2017-12-31 15:40:33 -0700

    Bug 1427505: Move `MOZ_SUITE` to `moz.configure`; r=frg
    MozReview-Commit-ID: c3vIfmN34u

--- suite/confvars.sh
+++ suite/confvars.sh
@@ -9,7 +9,6 @@ MOZ_APP_BASENAME=SeaMonkey
 MOZ_APP_VENDOR=Mozilla
 MOZ_APP_NAME=seamonkey
 MOZ_APP_DISPLAYNAME=SeaMonkey
-MOZ_SUITE=1
 MOZ_BRANDING_DIRECTORY=comm/suite/branding/seamonkey
 MOZ_OFFICIAL_BRANDING_DIRECTORY=comm/suite/branding/seamonkey
 
