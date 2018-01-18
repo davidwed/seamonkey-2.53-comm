b5de8aa1f81ac0a0464d163e6850a903ceb5ec2d
rene <just@absorb.it>
2018-01-18 14:11:02 +0100

    Bug 1294027 - use mail sender's address for SMTP MAIL FROM. r=jorgk

--- mailnews/compose/src/nsMsgSend.cpp
+++ mailnews/compose/src/nsMsgSend.cpp
@@ -3413,7 +3413,7 @@ nsMsgComposeAndSend::DeliverFileAsMail()
       msgStatus = do_QueryInterface(mStatusFeedback);
 
     nsCOMPtr<nsIURI> runningUrl;
-    rv = smtpService->SendMailMessage(mTempFile, buf, mUserIdentity,
+    rv = smtpService->SendMailMessage(mTempFile, buf, mUserIdentity, mCompFields->GetFrom(),
                                       mSmtpPassword, deliveryListener, msgStatus,
                                       callbacks, mCompFields->GetDSN(),
                                       getter_AddRefs(runningUrl),
