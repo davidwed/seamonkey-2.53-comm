6d57d0f6336d605dfd9b4a26e2dfb870727b5004
Jorg K <jorgk@jorgk.com>
2018-02-02 01:03:12 +0100

    Bug 1435093 - Port bug 1421084 to mailnews: remove nsNSSShutDownPreventionLock, isAlreadyShutDown, shutdown, virtualDestroyNSSReference, nsNSSShutDown.h from mailnews. rs=bustage-fix

--- mailnews/mime/src/nsCMSSecureMessage.cpp
+++ mailnews/mime/src/nsCMSSecureMessage.cpp
@@ -21,7 +21,6 @@
 #include "nsIX509CertDB.h"
 #include "nsNSSComponent.h"
 #include "nsNSSHelper.h"
-#include "nsNSSShutDown.h"
 #include "plbase64.h"
 
 #ifdef PR_LOGGING
@@ -98,7 +97,6 @@ NS_IMETHODIMP nsCMSSecureMessage::CanBeUsedForEmailSigning(nsIX509Cert* aCert, b
 NS_IMETHODIMP nsCMSSecureMessage::
 DecodeCert(const char *value, nsIX509Cert ** _retval)
 {
-  nsNSSShutDownPreventionLock locker;
   MOZ_LOG(gPIPNSSLog, LogLevel::Debug, ("nsCMSSecureMessage::DecodeCert\n"));
   nsresult rv = NS_OK;
   int32_t length;
@@ -138,7 +136,6 @@ DecodeCert(const char *value, nsIX509Cert ** _retval)
 NS_IMETHODIMP nsCMSSecureMessage::
 SendMessage(const char *msg, const char *base64Cert, char ** _retval)
 {
-  nsNSSShutDownPreventionLock locker;
   MOZ_LOG(gPIPNSSLog, LogLevel::Debug, ("nsCMSSecureMessage::SendMessage\n"));
   nsresult rv = NS_OK;
   CERTCertificate *cert = 0;
@@ -262,7 +259,6 @@ done:
 NS_IMETHODIMP nsCMSSecureMessage::
 ReceiveMessage(const char *msg, char **_retval)
 {
-  nsNSSShutDownPreventionLock locker;
   MOZ_LOG(gPIPNSSLog, LogLevel::Debug, ("nsCMSSecureMessage::ReceiveMessage\n"));
   nsresult rv = NS_OK;
   NSSCMSDecoderContext *dcx;
