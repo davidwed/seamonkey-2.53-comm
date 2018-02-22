fb1a20fa8168aa40f208e82161e0473bd1ad2986
Philipp Kewisch <mozilla@kewis.ch>
2018-02-22 07:35:59 +0100

    Bug 1440490 - Move l10n related functions into calL10NUtils.jsm - part 1 - initial manual changes. r=MakeMyDay
    MozReview-Commit-ID: 9OhRVmaDaZj

--- calendar/.eslintrc.js
+++ calendar/.eslintrc.js
@@ -483,6 +483,7 @@ module.exports = {
             "base/modules/calEmailUtils.jsm",
             "base/modules/calItipUtils.jsm",
             "base/modules/calUnifinderUtils.jsm",
+            "base/modules/calL10NUtils.jsm",
         ],
         rules: {
             "require-jsdoc": [2, { require: { ClassDeclaration: true } }],
