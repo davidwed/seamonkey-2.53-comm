9f7e2451eea372daffc8e979174add15574b4c25
Marco Bonardo <mbonardo@mozilla.com>
2018-05-09 12:08:15 +0200

    Bug 1430023 - Stop using LStoreS in comm-central. r=jorgk a=frg

--- suite/base/content/safeMode.js
+++ suite/base/content/safeMode.js
@@ -28,14 +28,11 @@ function restoreDefaultBookmarks() {
 }
 
 function deleteLocalstore() {
-  // Delete the old localstore otherwise it will get imported.
-  var localstoreFile = Services.dirsvc.get("LStoreS", Ci.nsIFile);
-  if (localstoreFile.exists())
-    localstoreFile.remove(false);
-  // Delete the new xulstore file.
-  localstoreFile.leafName = "xulstore.json";
-  if (localstoreFile.exists())
-    localstoreFile.remove(false);
+  // Delete the xulstore file.
+  let xulstoreFile = Services.dirsvc.get("ProfD", Ci.nsIFile);
+  xulstoreFile.append("xulstore.json");
+  if (xulstoreFile.exists())
+    xulstoreFile.remove(false);
 }
 
 function disableAddons() {
