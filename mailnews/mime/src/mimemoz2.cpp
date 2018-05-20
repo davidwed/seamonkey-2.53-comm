841a659e84594bf44b8531407bd548e0381358e1
Jorg K <jorgk@jorgk.com>
2018-05-21 00:19:38 +0200

    Bug 1462481 - Follow-up: fix "Context-Type", fix typo in comment and remove comment referencing future code. r=me a=jorgk

--- mailnews/mime/src/mimemoz2.cpp
+++ mailnews/mime/src/mimemoz2.cpp
@@ -2076,7 +2076,6 @@ HTML2Plaintext(const nsString& inString, nsString& outString,
 /**
  * This function synchronously sanitizes an HTML document (string->string)
  * using the Gecko nsTreeSanitizer.
- * Compare HTMLSanitizeQuote() in nsMsgCompose.cpp.
  */
 nsresult
 HTMLSanitize(const nsString& inString, nsString& outString)
