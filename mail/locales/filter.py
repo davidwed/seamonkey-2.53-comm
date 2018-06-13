f4378265685635aa96d1f827d5040b63f7a0aabd
Michael Kaply <mozilla@kaply.com>
2018-06-13 13:36:19 -0700

    Bug 1468240 - Move searchplugins to mail directory. rs=jorgk DONTBUILD

--- mail/locales/filter.py
+++ mail/locales/filter.py
@@ -21,7 +21,4 @@ def test(mod, path, entity=None):
                          re.match(r"mail\.addr_book\.mapit_url\.[1-5]", entity))
             else "error")
 
-  # ignore search plugins
-  if re.match(r"searchplugins\/.+\.xml", path):
-    return "ignore"
   return "error"
