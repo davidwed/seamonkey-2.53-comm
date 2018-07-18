b52bb38e7f08c57641a9cb1de5c0317d10311c7f
MakeMyDay <makemyday@gmx-topmail.de>
2018-07-18 14:31:21 +0200

    Bug 1101381 - Restore Calendar printing. r=philipp DONTBUILD

--- calendar/import-export/calHtmlExport.js
+++ calendar/import-export/calHtmlExport.js
@@ -88,7 +88,8 @@ calHtmlExporter.prototype = {
             } else {
                 let row = itemNode.querySelector(".intervalrow");
                 row.remove();
-                if (row.nextSibling instanceof Components.interfaces.nsIDOMText) {
+                if (row.nextSibling &&
+                    row.nextSibling instanceof Components.interfaces.nsIDOMText) {
                     row.nextSibling.remove();
                 }
             }
