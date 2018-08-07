fa9ca66e50addf34e63fff3227e34187145989bc
Jorg K <jorgk@jorgk.com>
2018-08-07 09:47:31 +0200

    Bug 1481326 - fix some clang-cl warnings and allow others. rs=bustage-fix

--- mailnews/addrbook/src/nsWabAddressBook.cpp
+++ mailnews/addrbook/src/nsWabAddressBook.cpp
@@ -6,6 +6,7 @@
 #include <tchar.h>
 #include "nsWabAddressBook.h"
 #include "mozilla/Logging.h"
+#include "mozilla/DebugOnly.h"
 #include <algorithm>
 
 static mozilla::LazyLogModule gWabAddressBookLog("nsWabAddressBookLog");
@@ -82,7 +83,7 @@ void nsWabAddressBook::FreeWabLibrary(void)
 nsWabAddressBook::nsWabAddressBook(void)
 : nsAbWinHelper()
 {
-    BOOL result = Initialize() ;
+    mozilla::DebugOnly<BOOL> result = Initialize() ;
 
     NS_ASSERTION(result == TRUE, "Couldn't initialize Wab Helper") ;
     MOZ_COUNT_CTOR(nsWabAddressBook) ;
