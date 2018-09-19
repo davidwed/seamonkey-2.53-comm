1ec6a086b162f6840d15428d031ceeacd9a8a548
MakeMyDay <makemyday@gmx-topmail.de>
2018-09-19 14:17:49 +0200

    Bug 1492436 - Delete a recurring item if the last occurrence is deleted; r=philipp

--- calendar/lightning/content/lightning-item-iframe.js
+++ calendar/lightning/content/lightning-item-iframe.js
@@ -3070,11 +3070,19 @@ function onCommandDeleteItem() {
         eventDialogCalendarObserver.cancel();
         if (window.calendarItem.parentItem.recurrenceInfo && window.calendarItem.recurrenceId) {
             // if this is a single occurrence of a recurring item
-            let newItem = window.calendarItem.parentItem.clone();
-            newItem.recurrenceInfo.removeOccurrenceAt(window.calendarItem.recurrenceId);
-
-            gMainWindow.doTransaction("modify", newItem, newItem.calendar,
-                                      window.calendarItem.parentItem, deleteListener);
+            if (countOccurrences(window.calendarItem) == 1) {
+                // this is the last occurrence, hence we delete the parent item
+                // to not leave a parent item without children in the calendar
+                gMainWindow.doTransaction("delete", window.calendarItem.parentItem,
+                                          window.calendarItem.calendar, null,
+                                          deleteListener);
+            } else {
+                // we just need to remove the occurrence
+                let newItem = window.calendarItem.parentItem.clone();
+                newItem.recurrenceInfo.removeOccurrenceAt(window.calendarItem.recurrenceId);
+                gMainWindow.doTransaction("modify", newItem, newItem.calendar,
+                                          window.calendarItem.parentItem, deleteListener);
+            }
         } else {
             gMainWindow.doTransaction("delete", window.calendarItem, window.calendarItem.calendar,
                                       null, deleteListener);
