2e58684ef08aac5f54bab8a781249198053430df
Geoff Lankow <geoff@darktrojan.net>
2018-11-28 10:29:36 +1300

    Bug 1508942 - Remove more plugins code; disable plugins by content policy. r=mkmelin

--- mail/components/preferences/actionsshared.js
+++ mail/components/preferences/actionsshared.js
@@ -7,9 +7,7 @@ var FILEACTION_SAVE_TO_DISK     = 1;
 var FILEACTION_OPEN_INTERNALLY  = 2;
 var FILEACTION_OPEN_DEFAULT     = 3;
 var FILEACTION_OPEN_CUSTOM      = 4;
-var FILEACTION_OPEN_PLUGIN      = 5;
-function FileAction ()
-{
+function FileAction () {
 }
 FileAction.prototype = {
   type        : "",
@@ -23,9 +21,4 @@ FileAction.prototype = {
   mimeInfo    : null,
   customHandler       : "",
   handleMode          : false,
-  pluginAvailable     : false,
-  pluginEnabled       : false,
-  handledOnlyByPlugin : false
 };
-
-
