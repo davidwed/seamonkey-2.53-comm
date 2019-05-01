c3c5b2976cf88843b9c05ed4d480b83d3c828342
aceman <acelists@atlas.sk>
2019-05-01 19:25:34 +0200

    Bug 1510571 - clean up some includes of nsMsgMessageFlags.h and use of Ci.nsMsgMessageFlags. r=jorgk

--- mailnews/compose/src/nsMsgSend.cpp
+++ mailnews/compose/src/nsMsgSend.cpp
@@ -69,6 +69,7 @@
 #include "nsIMsgMessageService.h"
 #include "nsIMsgHdr.h"
 #include "nsIMsgFolder.h"
+#include "nsMsgMessageFlags.h"
 #include "nsComposeStrings.h"
 #include "nsString.h"
 #include "nsMsgUtils.h"
