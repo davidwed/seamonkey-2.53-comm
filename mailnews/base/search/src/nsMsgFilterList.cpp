52b7f72144e6e25f3b1c09b7275fd767838c3be1
Jorg K <jorgk@jorgk.com>
2019-07-08 00:21:10 +0200

    Bug 1563959 - Check that we got a non-null header before running a filter on it (and crashing). r=aceman

--- mailnews/base/search/src/nsMsgFilterList.cpp
+++ mailnews/base/search/src/nsMsgFilterList.cpp
@@ -291,6 +291,14 @@ nsMsgFilterList::ApplyFiltersToHdr(nsMsgFilterTypeType filterType,
 {
   MOZ_LOG(FILTERLOGMODULE, LogLevel::Debug,
           ("(Auto) nsMsgFilterList::ApplyFiltersToHdr"));
+  if (!msgHdr) {
+    // Sometimes we get here with no header, so let's not crash on that
+    // later on.
+    MOZ_LOG(FILTERLOGMODULE, LogLevel::Debug,
+            ("(Auto) Called with NULL message header, nothing to do"));
+    return NS_ERROR_NULL_POINTER;
+  }
+
   nsCOMPtr<nsIMsgFilter> filter;
   uint32_t filterCount = 0;
   nsresult rv = GetFilterCount(&filterCount);
@@ -343,7 +351,7 @@ nsMsgFilterList::ApplyFiltersToHdr(nsMsgFilterTypeType filterType,
                  NS_ConvertUTF16toUTF8(filterName).get()));
 
         nsresult matchTermStatus = NS_OK;
-        bool result;
+        bool result = false;
 
         filter->SetScope(scope);
         matchTermStatus = filter->MatchHdr(msgHdr, folder, db, headers, &result);
