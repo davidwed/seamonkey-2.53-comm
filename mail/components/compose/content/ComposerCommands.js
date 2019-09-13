a7f289d1378bf0e5f245572d76d324ea6f837704
Khushil Mistry <khushil324@gmail.com>
2019-09-13 18:01:02 +0200

    Bug 1581090 - remove EdInputImage.xul and related JS and DTD files from compose directory. r=mkmelin

--- mail/components/compose/content/ComposerCommands.js
+++ mail/components/compose/content/ComposerCommands.js
@@ -31,7 +31,6 @@ function SetupHTMLEditorCommands()
   commandTable.registerCommand("cmd_removeNamedAnchors", nsRemoveNamedAnchorsCommand);
   commandTable.registerCommand("cmd_editLink",        nsEditLinkCommand);
 
-  commandTable.registerCommand("cmd_inputimage",    nsInputImageCommand);
   commandTable.registerCommand("cmd_isindex",       nsIsIndexCommand);
   commandTable.registerCommand("cmd_image",         nsImageCommand);
   commandTable.registerCommand("cmd_hline",         nsHLineCommand);
@@ -2367,23 +2366,6 @@ var nsValidateCommand =
 };
 
 //-----------------------------------------------------------------------------------
-var nsInputImageCommand =
-{
-  isCommandEnabled: function(aCommand, dummy)
-  {
-    return (IsDocumentEditable() && IsEditingRenderedHTML());
-  },
-
-  getCommandStateParams: function(aCommand, aParams, aRefCon) {},
-  doCommandParams: function(aCommand, aParams, aRefCon) {},
-
-  doCommand: function(aCommand)
-  {
-    window.openDialog("chrome://messenger/content/messengercompose/EdInputImage.xul", "_blank", "chrome,close,titlebar,modal");
-  }
-};
-
-//-----------------------------------------------------------------------------------
 var nsIsIndexCommand =
 {
   isCommandEnabled: function(aCommand, dummy)
@@ -2665,11 +2647,6 @@ var nsObjectPropertiesCommand =
           break;
         case 'hr':
           goDoCommand("cmd_hline");
-          break;
-        case 'input':
-          var type = element.getAttribute("type");
-          if (type && type.toLowerCase() == "image")
-            goDoCommand("cmd_inputimage");
           break;
         case 'table':
           EditorInsertOrEditTable(false);
