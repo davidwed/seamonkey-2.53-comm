6c2abb86010b0700e77a5c49f0fff3768588d396
Frank-Rainer Grahl <frgrahl@gmx.net>
2019-12-29 18:06:34 +0100

    Bug 1600448 - Follow-up for Bug 1434737. Replace ChromeUtils.import() part 2 in suite. r=IanN a=IanN

--- suite/components/pref/content/pref-download.js
+++ suite/components/pref/content/pref-download.js
@@ -3,12 +3,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");
 ChromeUtils.import("resource://gre/modules/Services.jsm");
 ChromeUtils.import("resource://gre/modules/FileUtils.jsm");
 
-XPCOMUtils.defineLazyModuleGetter(this, "Downloads",
-                                  "resource://gre/modules/Downloads.jsm");
+ChromeUtils.defineModuleGetter(this, "Downloads",
+  "resource://gre/modules/Downloads.jsm");
 
 const kDesktop = 0;
 const kDownloads = 1;
