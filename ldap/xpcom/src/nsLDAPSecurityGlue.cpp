8bdd919e393c4739397af5500bef2a5228a9579f
Magnus Melin <mkmelin+mozilla@iki.fi>
2020-06-10 15:42:24 +0300

    Bug 1610797 - Fix crash in [@ nsLDAPSSLClose]. r=benc

--- ldap/xpcom/src/nsLDAPSecurityGlue.cpp
+++ ldap/xpcom/src/nsLDAPSecurityGlue.cpp
@@ -60,15 +60,19 @@ nsLDAPSSLClose(int s, struct lextiof_socket_private *socketarg)
     memset(&socketInfo, 0, sizeof(socketInfo));
     socketInfo.soinfo_size = PRLDAP_SOCKETINFO_SIZE;
     if (prldap_get_socket_info(s, socketarg, &socketInfo) != LDAP_SUCCESS) {
-	NS_ERROR("nsLDAPSSLClose(): prldap_get_socket_info() failed\n");
-	return -1;
+      NS_ERROR("nsLDAPSSLClose(): prldap_get_socket_info() failed\n");
+      return -1;
     }
 
     // save off the session closure data in an automatic, since we're going to
     // need to call through it
     //
-    socketClosure = reinterpret_cast<nsLDAPSSLSocketClosure *>
-                                    (socketInfo.soinfo_appdata);
+    socketClosure =
+      reinterpret_cast<nsLDAPSSLSocketClosure *>(socketInfo.soinfo_appdata);
+    if (!socketClosure) {
+      NS_ERROR("nsLDAPSSLClose(): no socketClosure to be had");
+      return -1;
+    }
     sessionClosure = socketClosure->sessionClosure;
 
     // free the socket closure data
