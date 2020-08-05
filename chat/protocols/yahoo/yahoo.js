9786b7e0ef8bf4fa9d567221ed7d55968b78ace8
Patrick Cloke <clokep@gmail.com>
2020-08-05 13:03:31 +0300

    Bug 1445778 - Remove Twitter support due to disabled streaming API. r=khushil

--- chat/protocols/yahoo/yahoo.js
+++ chat/protocols/yahoo/yahoo.js
@@ -19,7 +19,7 @@ YahooAccount.prototype = {
   connect: function() {
     this.WARN("The legacy versions of Yahoo Messenger was disabled on August " +
               "5, 2016. It is currently not possible to connect to Yahoo " +
-              "Messenger. See bug 1316000");
+              "Messenger. See bug 1316000.");
     this.reportDisconnecting(Ci.prplIAccount.ERROR_OTHER_ERROR,
                              _("yahoo.disabled"));
     this.reportDisconnected();
