11a27bb9a7adca6afe3ef393811917b9385ae3db
Frank-Rainer Grahl <frgrahl@gmx.net>
2021-06-08 10:20:01 -0400

    Bug 1519902 - Use current document for imageCache.findEntryProperties. Port Bug 1202085 part 2 [Do not store images that have been synthesized by a service worker in the image cache].

--- suite/base/content/nsContextMenu.js
+++ suite/base/content/nsContextMenu.js
@@ -88,7 +88,7 @@ nsContextMenu.prototype = {
         let imageCache = Cc["@mozilla.org/image/tools;1"]
                            .getService(Ci.imgITools)
                            .getImgCacheForDocument(doc);
-        let props = imageCache.findEntryProperties(popupNode.currentURI);
+        let props = imageCache.findEntryProperties(popupNode.currentURI, doc);
         if (props) {
           let nsISupportsCString = Ci.nsISupportsCString;
           contentType = props.get("type", nsISupportsCString).data;
