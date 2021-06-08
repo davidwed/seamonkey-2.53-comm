f6fa426a494ae0a2f77107f467d6a7bbb4bdc820
Frank-Rainer Grahl <frgrahl@gmx.net>
2021-06-08 10:20:01 -0400

    Bug 1588875 - Detect OS using AppConstants.platform. r=IanN a=IanN

--- suite/components/pref/content/pref-content.js
+++ suite/components/pref/content/pref-content.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+ChromeUtils.import("resource://gre/modules/AppConstants.jsm");
+
 var minMinValue;
 var maxMinValue;
 
@@ -78,7 +80,7 @@ function AdjustMinZoom()
  */
 function updateHardwareAcceleration(aVal)
 {
-  if (/^Win/.test(navigator.platform)) {
+  if (AppConstants.platform == "win") {
     document.getElementById("gfx.direct2d.disabled").value = aVal;
   }
 }
