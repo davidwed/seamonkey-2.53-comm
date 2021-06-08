a2d3c4a9d716c4537419b490d9aeb8e5e73b2101
Ian Neal <iann_cvs@blueyonder.co.uk>
2021-06-08 10:20:01 -0400

    Bug 1475867 - Port |Bug 1238712 - Remove duplication between nsWindowsShellService, nsGnomeShellService, and nsMacShellService| to SeaMonkey r=frg

--- suite/base/content/defaultClientDialog.js
+++ suite/base/content/defaultClientDialog.js
@@ -3,27 +3,29 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
 // this dialog can only be opened if we have a shell service
-const nsIShellService = Components.interfaces.nsIShellService;
+XPCOMUtils.defineLazyModuleGetter(this, "ShellService",
+                                  "resource:///modules/ShellService.jsm");
+
 const nsIPrefBranch = Components.interfaces.nsIPrefBranch;
 
 function onLoad()
 {
-  var shellSvc = Components.classes["@mozilla.org/suite/shell-service;1"]
-                           .getService(nsIShellService);
   var defaultList = document.getElementById("defaultList");
-  var appTypes = shellSvc.shouldBeDefaultClientFor;
+  var appTypes = ShellService.shouldBeDefaultClientFor;
   /* Iterate through the list of possible default client types and check for
      each list item if we want to be the default for that type using the AND
      conjunction */
   for (var i = 0; i < defaultList.getRowCount(); i++) {
     var currentItem = defaultList.getItemAtIndex(i);
     try {
-      if (shellSvc.isDefaultClient(false, nsIShellService[currentItem.value])) {
+      if (ShellService.isDefaultClient(false, Ci.nsIShellService[currentItem.value])) {
         currentItem.checked = true;
         currentItem.disabled = true;
       }
-      else if (nsIShellService[currentItem.value] & appTypes)
+      else if (Ci.nsIShellService[currentItem.value] & appTypes)
         currentItem.checked = true;
     } catch (e) {
       currentItem.hidden = true;
@@ -34,15 +36,13 @@ function onLoad()
 function onAccept()
 {
   // for each checked item, if we aren't already the default, make us the default.
-  var shellSvc = Components.classes["@mozilla.org/suite/shell-service;1"]
-                           .getService(nsIShellService);
   var appTypes = 0;
   var appTypesCheck = 0;
   var defaultList = document.getElementById("defaultList");
 
   for (var i = 0; i < defaultList.getRowCount(); i++) {
     var currentItem = defaultList.getItemAtIndex(i);
-    var currentAppType = nsIShellService[currentItem.value];
+    var currentAppType = Ci.nsIShellService[currentItem.value];
 
     if (currentItem.checked) {
       appTypesCheck |= currentAppType;
@@ -53,10 +53,10 @@ function onAccept()
   }
 
   if (appTypes)
-    shellSvc.setDefaultClient(false, true, appTypes);
+    ShellService.setDefaultClient(false, true, appTypes);
 
   // Update the pref for which app types we should check if we are the default app
-  shellSvc.shouldBeDefaultClientFor = appTypesCheck;
+  ShellService.shouldBeDefaultClientFor = appTypesCheck;
 
-  shellSvc.shouldCheckDefaultClient = document.getElementById('checkOnStartup').checked;
+  ShellService.shouldCheckDefaultClient = document.getElementById('checkOnStartup').checked;
 }
