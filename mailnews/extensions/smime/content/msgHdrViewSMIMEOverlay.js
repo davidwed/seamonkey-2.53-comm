0372df7ec81d81c09b5b04ba82c070698d4fcc37
Frank-Rainer Grahl <frgrahl@gmx.net>
2021-06-08 10:20:01 -0400

    Bug 1468079 - Port relevant parts of [Bug 1411592 - prevent remote content for encrypted S/MIME messages] to SeaMonkey.

--- mailnews/extensions/smime/content/msgHdrViewSMIMEOverlay.js
+++ mailnews/extensions/smime/content/msgHdrViewSMIMEOverlay.js
@@ -20,6 +20,17 @@ var gSMIMEBundle = null;
 
 var nsICMSMessageErrors = Components.interfaces.nsICMSMessageErrors;
 
+/// Get the necko URL for the message URI.
+function neckoURLForMessageURI(aMessageURI)
+{
+  let msgSvc = Components.classes["@mozilla.org/messenger;1"]
+    .createInstance(Components.interfaces.nsIMessenger)
+    .messageServiceFromURI(aMessageURI);
+  let neckoURI = {};
+  msgSvc.GetUrlForUri(aMessageURI, neckoURI, null);
+  return neckoURI.value.spec;
+}
+
 var smimeHeaderSink = 
 { 
   maxWantedNesting: function()
@@ -92,8 +103,11 @@ var smimeHeaderSink =
     
     if (gEncryptedURIService)
     {
+      // Remember the message URI and the corresponding necko URI.
       gMyLastEncryptedURI = GetLoadedMessage();
       gEncryptedURIService.rememberEncrypted(gMyLastEncryptedURI);
+      gEncryptedURIService.rememberEncrypted(
+        neckoURLForMessageURI(gMyLastEncryptedURI));
     }
 
     switch (aEncryptionStatus)
@@ -133,6 +147,8 @@ function forgetEncryptedURI()
   if (gMyLastEncryptedURI && gEncryptedURIService)
   {
     gEncryptedURIService.forgetEncrypted(gMyLastEncryptedURI);
+    gEncryptedURIService.forgetEncrypted(
+      neckoURLForMessageURI(gMyLastEncryptedURI));
     gMyLastEncryptedURI = null;
   }
 }
