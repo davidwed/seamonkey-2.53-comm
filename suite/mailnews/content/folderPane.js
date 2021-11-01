0937afcf2120dfa4f28a236312a09a4f28d82034
Nuno Silva <nunojsg@gmail.com>
2021-11-01 14:14:21 +0100

    Bug 1738669 - Correct typo in folderPane.js. r=frg a=frg

--- suite/mailnews/content/folderPane.js
+++ suite/mailnews/content/folderPane.js
@@ -2007,7 +2007,7 @@ var gFolderTreeController = {
 
     window.openDialog("chrome://messenger/content/virtualFolderProperties.xul",
                       "", "chrome,modal,centerscreen",
-                      {folder: folder, searchTerms: aSearchTems,
+                      {folder: folder, searchTerms: aSearchTerms,
                        newFolderName: name});
   },
 
