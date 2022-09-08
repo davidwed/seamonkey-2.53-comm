1b1d1235f1689ed1d4d9ed091b8b3cff8aa6b81b
Bill Gianopoulos <wgianopoulos@gmail.com>
2022-09-08 10:10:33 +0000

    Bug 1794394 - Remove PermissionsService from suite process Windows sandboxing code. r=frg a=frg Port Bug 1788233 "Remove PermissionsService from process Windows sandboxing code".

--- suite/app/nsSuiteApp.cpp
+++ suite/app/nsSuiteApp.cpp
@@ -212,8 +212,6 @@ static int do_main(int argc, char* argv[], char* envp[])
 #if defined(XP_WIN) && defined(MOZ_SANDBOX)
   sandbox::BrokerServices* brokerServices =
     sandboxing::GetInitializedBrokerServices();
-  sandboxing::PermissionsService* permissionsService =
-    sandboxing::GetPermissionsService();
 #if defined(MOZ_CONTENT_SANDBOX)
   if (!brokerServices) {
     Output("Couldn't initialize the broker services.\n");
@@ -221,7 +219,6 @@ static int do_main(int argc, char* argv[], char* envp[])
   }
 #endif
   config.sandboxBrokerServices = brokerServices;
-  config.sandboxPermissionsService = permissionsService;
 #endif
 
 #ifdef LIBFUZZER
