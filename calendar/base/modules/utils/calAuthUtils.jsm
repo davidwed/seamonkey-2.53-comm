c53mike/comm-central

Bug 1505950 - Ask to save password for CalDAV calendars; r=Fallen

calendar/base/modules/utils/calAuthUtils.jsm
@@ -104,10 +104,22 @@ var calauth = {
                 aAuthInfo.password = pwInfo.password;
                 return true;
             } else {
-                let prompter2 = Components.classes["@mozilla.org/embedcomp/window-watcher;1"]
-                                          .getService(Components.interfaces.nsIPromptFactory)
-                                          .getPrompt(this.mWindow, Components.interfaces.nsIAuthPrompt2);
-                return prompter2.promptAuth(aChannel, aLevel, aAuthInfo);
+                let savePasswordLabel = null;
+                if (Preferences.get("signon.rememberSignons", true)) {
+                    savePasswordLabel = cal.l10n.getAnyString("passwordmgr",
+                                                              "passwordmgr",
+                                                              "rememberPassword");
+                }
+                let savePassword = {};
+                let returnValue = Services.prompt.promptAuth(null, aChannel, aLevel, aAuthInfo,
+                                                             savePasswordLabel, savePassword);
+                if (savePassword.value) {
+                    calauth.passwordManagerSave(aAuthInfo.username,
+                                                aAuthInfo.password,
+                                                hostRealm.prePath,
+                                                aAuthInfo.realm);
+                }
+                return returnValue;
             }
         }
 

davidwed/seamonkey-2.53-comm

calendar/base/modules/utils/calAuthUtils.jsm
@@ -104,10 +104,22 @@ var calauth = {
                 aAuthInfo.password = pwInfo.password;
                 return true;
             } else {
-                let prompter2 = Components.classes["@mozilla.org/embedcomp/window-watcher;1"]
-                                          .getService(Components.interfaces.nsIPromptFactory)
-                                          .getPrompt(this.mWindow, Components.interfaces.nsIAuthPrompt2);
-                return prompter2.promptAuth(aChannel, aLevel, aAuthInfo);
+                let savePasswordLabel = null;
+                if (Preferences.get("signon.rememberSignons", true)) {
+                    savePasswordLabel = cal.l10n.getAnyString("passwordmgr",
+                                                              "passwordmgr",
+                                                              "rememberPassword");
+                }
+                let savePassword = {};
+                let returnValue = Services.prompt2.promptAuth(null, aChannel, aLevel, aAuthInfo,
+                                                              savePasswordLabel, savePassword);
+                if (savePassword.value) {
+                    calauth.passwordManagerSave(aAuthInfo.username,
+                                                aAuthInfo.password,
+                                                hostRealm.prePath,
+                                                aAuthInfo.realm);
+                }
+                return returnValue;
             }
         }
 